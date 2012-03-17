aff38fa5801c5733dde61626d8387ab4b8090b20
Jakob Dam <jakob.a.dam@gmail.com>
2012-03-17 18:47:00 +0100

    saving last run of task.

--- cron/cron.py
+++ cron/cron.py
@@ -4,7 +4,7 @@ from datetime import date
 from datetime import datetime
 
 import paas
-from models import Job
+from models import Job, db
 import urllib2
 import mail
 
@@ -16,5 +16,9 @@ jobs = Job.query.filter(Job.last_run < today).filter(Job.hour >= now.hour).all()
 for j in jobs:
     try:
         urllib2.urlopen(j.url).read()
+        j.last_run = now
+        db.session.add(j)
     except Exception, e:
         mail.send(receivers=[j.user.email], subject=u'Fejl ved kørsel af job', text=str(e))
+
+db.session.commit()
