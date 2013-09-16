aa2bd78d0eae9c5dc2b19767fa3f15ef4b9c0387
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-09-16 09:35:06 +0200

    Disable mail in dev. Logging mail instead.

--- mail.py
+++ mail.py
@@ -7,7 +7,7 @@ from email.Header import Header
 
 from app import app
 
-EMAIL_FROM = app.config.get('EMAIL_FROM') 
+EMAIL_FROM = app.config.get('EMAIL_FROM')
 EMAIL_SERVER = app.config.get('EMAIL_SERVER')
 EMAIL_ADMINS = app.config.get('ADMINS')
 
@@ -21,26 +21,25 @@ def send(sender=EMAIL_FROM, receivers=None, subject='', text='', html=''):
         msg['From'] = encode_header(EMAIL_FROM)
         receivers = [encode_header(r) for r in receivers]
         msg['To'] = ' ,'.join(receivers)
-        
+
         part1 = MIMEText(text.encode('utf-8'), 'plain', 'utf-8')
         part2 = MIMEText(html.encode('utf-8'), 'html', 'utf-8')
-        
+
         if text:
             msg.attach(part1)
         if html:
             msg.attach(part2)
-        
-        server = smtplib.SMTP(EMAIL_SERVER)
-        if app.config['MODE'] != 'test':
+
+        if app.config['MODE'] == 'production':
+            server = smtplib.SMTP(EMAIL_SERVER)
             server.sendmail(sender, receivers, msg.as_string())
             logging.info('mail: sending mail %s to %s from %s' % (msg['Subject'], msg['From'], msg['To']))
+            server.quit()
         else:
             logging.info('mail: skipping sending mail %s to %s from %s' % (msg['Subject'], msg['From'], msg['To']))
-        server.quit()
+            logging.info(text)
     except Exception, e:
         logging.error('mail error: %s' % e)
 
 def send_admin(subject='', text='', html=''):
     send(receivers=EMAIL_ADMINS, subject=subject, text=text, html=html)
-
-    
