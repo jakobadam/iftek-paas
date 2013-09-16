fe9c2f4fe9275a03983d90d72742cf7cd3052b4d
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-09-16 09:41:31 +0200

    logging in dev.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import logging
 from logging.handlers import SMTPHandler
 
+
 from flask import Flask
 app = Flask('paas')
 
@@ -12,7 +13,7 @@ site_root = os.path.dirname(os.path.abspath(__file__))
 if os.environ.get('SETTINGS'):
     # Another way to enable production state do this:
     # $ export SETTINGS=./conf/settings.cfg
-    app.config.from_envvar('SETTINGS')    
+    app.config.from_envvar('SETTINGS')
 else:
     app.config.from_pyfile(os.path.join(site_root, 'conf', 'settings.dev.cfg'))
 
@@ -24,3 +25,6 @@ if app.config['MODE'] == 'production':
                                app.config['ADMINS'], 'skyen.iftek.dk failed')
     mail_handler.setLevel(logging.ERROR)
     logging.getLogger().addHandler(mail_handler)
+else:
+    l = logging.getLogger()
+    l.setLevel(logging.DEBUG)
