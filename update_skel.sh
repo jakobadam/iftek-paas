7d711cb896aa15c24e4ce8a67d4bbf0c830f3e96
Jakob A. Dam <jakob.a.dam@gmail.com>
2013-10-09 21:29:56 +0200

    helper to update skel.

---
+++ update_skel.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+rm -rf /etc/skel/public_html
+mkdir  /etc/skel/public_html
+
+rm -rf /tmp/iftek
+git clone https://github.com/jakobadam/iftek.git /tmp/iftek
+cd /tmp/iftek
+git archive master | tar -x -C /etc/skel/public_html
