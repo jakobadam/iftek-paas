16f026c4b83fc2cf145db8009d05a5dd7059b1fb
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-12-11 23:02:17 +0100

    Script to add user.

--- useradd.py
+++ useradd.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 import sys
 import paas
-import models
+from models import User, db
 
 from flask import Flask
 
 if len(sys.argv) < 3:
-    sys.exit('useradd.py <user> <pwd>')
+    sys.exit('useradd.py <user> <pwd> <email>')
 
 app = Flask('paas.py')
 with app.app_context():
     user = sys.argv[1]
     password = sys.argv[2]
-    models.User.useradd(user, password)
-    print user
+    email = sys.argv[3]
+
+    # add linux and mysql user
+    User.useradd(user, password)
+
+    # add user to skyen db
+    user = User(username=user, password=password, email=email)
+    db.session.add(user)
+    db.session.commit()
+
+    print 'user added'
