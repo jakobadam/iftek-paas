92b69e88a52eb0c06c87a7222993d569afdb7a77
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-12-11 23:01:36 +0100

    Delete user completely.

--- userdel.sh
+++ userdel.sh
@@ -2,10 +2,10 @@
 USER=$1
 read -s -p "Enter MySQL Password: " PASSWORD
 userdel -r $1
-sql="DROP USER $USER;"
-echo $sql | mysql -u root --password="$PASSWORD"
-sql="DROP DATABASE \`$USER.blog\`;"
-echo $sql | mysql -u root --password="$PASSWORD"
-sql="DELETE FROM skyen.users WHERE username = '$1';"
-echo $sql | mysql -u root --password="$PASSWORD"
+
+echo "DROP USER $USER;" | mysql -u root --password="$PASSWORD"
+echo "DROP DATABASE \`$USER.blog\`;" | mysql -u root --password="$PASSWORD"
+echo "DELETE FROM skyen.validation_tokens \
+WHERE user_id = (select id from skyen.users where username = '$1');" | mysql -u root --password="$PASSWORD"
+echo "DELETE FROM skyen.users WHERE username = '$1';" | mysql -u root --password="$PASSWORD"
   
